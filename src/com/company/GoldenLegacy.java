f57d8ddd21032336b754697ffb7a3b6439ccfe4f
Ann <xhjfurb11@gmail.com>
2021-12-01 01:58:52 +0300

    change of code

--- src/com/company/GoldenLegacy.java
+++ src/com/company/GoldenLegacy.java
@@ -3,5 +3,24 @@ package com.company;
 class GoldenLegacy {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
